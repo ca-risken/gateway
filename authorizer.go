a312d75b61567322c51f05bfae03894737733262
gassara-kys <ogasawara_kiyoshi@cybernagent>
2023-12-26 18:20:09 +0900

    fix: store projectID for API token

--- authorizer.go
+++ authorizer.go
@@ -27,7 +27,8 @@ type requestUser struct {
 	name   string
 
 	// program access
-	accessTokenID uint32
+	accessTokenID        uint32
+	accessTokenProjectID uint32
 }
 
 func getRequestUser(r *http.Request) (*requestUser, error) {
@@ -183,7 +184,10 @@ func (g *gatewayService) authnToken(next http.Handler) http.Handler {
 			return
 		}
 		next.ServeHTTP(w, r.WithContext(
-			context.WithValue(ctx, userKey, &requestUser{accessTokenID: accessTokenID})))
+			context.WithValue(ctx, userKey, &requestUser{
+				accessTokenID:        accessTokenID,
+				accessTokenProjectID: projectID,
+			})))
 	}
 	return http.HandlerFunc(fn)
 }
@@ -338,20 +342,12 @@ func (g *gatewayService) authzProject(u *requestUser, r *http.Request) bool {
 
 func (g *gatewayService) authzProjectForToken(u *requestUser, r *http.Request) bool {
 	ctx := r.Context()
-	if zero.IsZeroVal(u.accessTokenID) {
-		return false
-	}
-	p := &requestProject{}
-	err := bind(p, r)
-	if err != nil {
-		appLogger.Warnf(ctx, "Failed to bind request, err=%+v", err)
-	}
-	if zero.IsZeroVal(p.ProjectID) {
+	if u.accessTokenID == 0 || u.accessTokenProjectID == 0 {
 		return false
 	}
 	req := &iam.IsAuthorizedTokenRequest{
 		AccessTokenId: u.accessTokenID,
-		ProjectId:     p.ProjectID,
+		ProjectId:     u.accessTokenProjectID,
 		ActionName:    getActionNameFromURI(r.URL.Path),
 		ResourceName:  getServiceNameFromURI(r.URL.Path) + "/resource_any",
 	}
